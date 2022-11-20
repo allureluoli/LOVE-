10c36206ac1dd04d78d71f3faba494ac60d1a6de
allureluoli <3345483363@qq.com>
2022-11-20 16:07:34 +0800

    抓兔机

--- love/plugins/love2/data/2.py
+++ love/plugins/love2/data/2.py
@@ -1,5 +1,28 @@
-from PIL import Image
+from urllib import request, error,parse
+from bs4 import BeautifulSoup
 
+def home_list(game_list):
+    url = "http://fgo-cdn.vgtime.com/media/fgo/servant/head/"
+    headers = {
+        "Accept": r"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+        "Connection": "keep-alive",
+        "Host": "www.corrodinggames.com",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0",
+        "accept-encoding":"identity"
+    }
 
+    dict = {
+        "name": "Germey"
+    }
+    data = bytes(parse.urlencode(dict), encoding="utf8")
 
-im=Image.open('http://fgo-cdn.vgtime.com/media/fgo/servant/card/367A.png',"r")
+    try:
+        req = request.Request(url=url, data=data, headers=headers, method="POST")
+        response = request.urlopen(req)
+        zfc1=response.read()
+        soup = BeautifulSoup(zfc1,"html.parser")
+    except:pass
+
--- love/plugins/love2/image/__init__.py
+++ love/plugins/love2/image/__init__.py
@@ -50,6 +50,52 @@ async def handle_func(bot: Bot, event: Event):
     image = MessageSegment.image(wtm)
     await tu.finish(image)
 
+
+tu_3=on_fullmatch(['FGO付费十连'],priority=50)
+@tu_3.handle()
+async def handle_func(bot: Bot, event: Event):
+    cxk=0
+    imageend = ""
+    while cxk <10:
+        one_star = [341, 294, 259, 257, 254, 174, 53, 50, 45, 39, 36, 16, 107]
+        two_star = [260, 258, 256, 255, 57, 54, 44, 43, 40, 34, 33, 25, 24, 21, 19]
+        three_star = [7, 9, 13, 15, 17, 20, 22, 23, 26, 27, 28, 31, 32, 35, 38, 42, 49, 55, 56, 63, 64, 71, 72, 79, 80,
+                      81, 95, 104, 105, 110, 117, 124, 125, 126, 148, 172, 186, 203, 204, 210, 231, 246, 249, 251, 273,
+                      348, 352]
+
+        cxk +=1
+        rnd3 = random.Random()
+        lucknum3 = rnd3.randint(1, 100)
+        if lucknum3 <=25:
+            rnd3 = random.Random()
+            x = rnd3.randint(1, 12)
+            lucknum=one_star[x]
+        elif lucknum3 <=35:
+            rnd3 = random.Random()
+            x = rnd3.randint(1, 14)
+            lucknum = two_star[x]
+        elif lucknum3 <= 45:
+            rnd3 = random.Random()
+            x = rnd3.randint(1, 46)
+            lucknum = three_star[x]
+        else:
+            rnd2 = random.Random()
+            lucknum = rnd2.randint(1, 367)
+
+        #一星爆率50 二星爆率30 三星爆率25 剩下百分之五去好池子里面抽
+
+        if lucknum >= 100:
+            wtm = "http://fgo-cdn.vgtime.com/media/fgo/servant/head/" + str(lucknum) + ".jpg"
+        elif lucknum >= 10:
+            wtm = "http://fgo-cdn.vgtime.com/media/fgo/servant/head/0" + str(lucknum) + ".jpg"
+        else:
+            wtm = "http://fgo-cdn.vgtime.com/media/fgo/servant/head/00" + str(lucknum) + ".jpg"
+
+        image = MessageSegment.image(wtm)
+        imageend += image
+
+    await tu_3.send(imageend)
+
 tu_2=on_fullmatch(['FGO十连'],priority=50)
 @tu_2.handle()
 async def handle_func(bot: Bot, event: Event):
@@ -95,10 +141,33 @@ async def handle_func(bot: Bot, event: Event):
 
     await tu_2.send(imageend)
 
+txztj = on_fullmatch(['铁锈抓兔机'], priority=50)
+
+@txztj.handle()
+async def handle_func(bot: Bot, event: Event):
+    File_1=open(FilePath(r"\data\铁锈单位.txt"),encoding="UTF-8")
+    list_1=File_1.read().replace("\n","").split("→")
+    rnd3 = random.Random()
+    x = rnd3.randint(0, 200)
+    try:
+        await txztj.send(f"恭喜你抓到了【{list_1[x]}】捏!将它抱回家吧！")
+    except:
+        await txztj.send("没有抓到捏~~嘿嘿！")
+
+txztj2 = on_fullmatch(['战队抓兔机'], priority=50)
+
+@txztj2.handle()
+async def handle_func(bot: Bot, event: Event):
+    File_1=open(FilePath(r"\data\铁锈单位2.txt"),encoding="UTF-8")
+    list_1=File_1.read().replace("\n","").split("→")
+    rnd3 = random.Random()
+    x = rnd3.randint(0, 250)
+    try:
+        await txztj2.send(f"恭喜你抓到了【{list_1[x]}】捏!将它抱回家吧！")
+    except:
+        await txztj2.send("没有抓到捏~~嘿嘿！")
 
 
-
-    #他欧了
 
 
 tu3=on_fullmatch(['早','早安','安','中午好','晚上好','午安','晚安','半夜好','凌晨好','睡了'],priority=50)
@@ -133,6 +202,13 @@ async def handle_func(bot: Bot, event: Event):
     image = MessageSegment.image(path)
     await tu7.finish(image)
 
+read_book=on_fullmatch(['看书','读书'],priority=50)
+@read_book.handle()
+async def handle_func(bot: Bot, event: Event):
+    path = Path(FilePath(r"\data\read_book.jpg")).parent / "read_book.jpg"
+    # 构造图片消息段
+    image = MessageSegment.image(path)
+    await read_book.finish(image)
 
 jrrp=on_command('今日抽签',priority=50)
 @jrrp.handle()
