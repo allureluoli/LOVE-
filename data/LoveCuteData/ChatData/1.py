17f84b57295df3c6b71eab0b3c0982d08eb0a9d5
allureluoli <3345483363@qq.com>
2023-09-15 04:32:20 +0800

    Update

--- data/LoveCuteData/ChatData/1.py
+++
@@ -1,8 +0,0 @@
-import json
-with open('new-data.json',encoding='utf-8') as f:
-    a = json.load(f)
-
-    a['娱乐词汇']['测试'] = '测试呢'
-
-with open('new-data.json',encoding='utf-8',mode='w') as f:
-    json.dump(a,f)
